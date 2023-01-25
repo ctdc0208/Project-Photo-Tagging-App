3dc98127b914af970bbeadf947d127f37a3851ec
nexus0208 <christiandelacruz0208@gmail.com>
2023-01-25 23:32:57 +0800

    Start navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-    const [count, setCount] = useState(0)
-
-    return ( 
+function App() {   
+  return ( 
     <div className='background-opacity'>
         <div className='main-container'>
             <div className='square'>
             </div>
-            <div className='navbar'>
-                Navbar
+            <div className='navbar navbar-divider'>
+                <div className='title margin-left-20px'>
+                    Photo Tagging App
+                </div>
+                    <div className='find-title margin-left-20px'>
+                        Find:
+                    </div>
+                    <div className='find-container margin-left-20px'>
+                        <div className='find-logo'>Logo</div>
+                        <div>Spiderman</div>
+                    </div>
             </div>
         </div>
     </div>
