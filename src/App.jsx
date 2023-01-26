7c79a5f2b23959ef1407a12323529708c7d11a55
nexus0208 <christiandelacruz0208@gmail.com>
2023-01-26 22:08:07 +0800

    Finish navbar layout - ver 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import './App.css'
+
 
 function App() {   
   return ( 
@@ -14,9 +14,30 @@ function App() {
                     <div className='find-title margin-left-20px'>
                         Find:
                     </div>
-                    <div className='find-container margin-left-20px'>
-                        <div className='find-logo'>Logo</div>
-                        <div>Spiderman</div>
+                    <div className='find-main-container'> 
+                        <div className='find-container margin-left-20px'>
+                            <div className='find-logo angry-bird'></div>
+                            <div>Red J. Bird</div>
+                        </div>
+                        <div className='find-container margin-left-20px'>
+                            <div className='find-logo superman'></div>
+                            <div>Superman</div>
+                        </div>
+                        <div className='find-container margin-left-20px'>
+                            <div className='find-logo mario'></div>
+                            <div>Mario</div>
+                        </div>
+                        <div className='find-container margin-left-20px'>
+                            <div className='find-logo meeseeks'></div>
+                            <div>Mr. Meeseeks</div>
+                        </div>
+                    </div>
+                    <div className='time'>
+                        <div className='margin-left-20px'>Time:</div>
+                        <div className='time-digits'>24:00</div>
+                        <button className='reset'>
+                        <svg className='reset' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>reload</title><path d="M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z" /></svg>
+                        </button>
                     </div>
             </div>
         </div>
