a06b759082a0f1240198776c68045cd2892dca7a
nexus0208 <christiandelacruz0208@gmail.com>
2023-02-15 21:47:22 +0800

    initial boardData

---
+++ src/Utilities/BoardData.js
@@ -0,0 +1,16 @@
+// import Board jpeg
+
+const boardData = {
+    0: {
+        level: 0,
+        name: "Easy",
+        board: 0,  // board jpeg
+        characters: [
+            {
+
+            }
+        ]
+    }
+};
+
+export default boardData;
