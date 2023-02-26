55abcfdd0c7e70548a97bcb3f748c76ac377b19c
nexus0208 <christiandelacruz0208@gmail.com>
2023-02-26 19:04:18 +0800

    initial game.js

---
+++ src/Game.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect} from "react";
+import CoordTwoDegrees from "./Utilities/CoordTwoDegrees";
+import OutsideClickHandler from "react-outside-click-handler/build/OutsideClickHandler";
+import boardData from "./Utilities/BoardData";
+
